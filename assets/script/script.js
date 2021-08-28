6a86d2cf704e84e2d08002c0717b7d5ba8de4fef
AlexeyAbramov <abramov.alexey.93@ya.ru>
2021-08-28 23:29:45 +0300

    added modal.html

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-  $('.body-award__wrapper').on('click',function(){
-    $('.answers').toggleClass('show')
-  })
+  $(".body-award__wrapper").on("click", function () {
+    $(".answers").toggleClass("show");
+  });
+
+  const textarea = document.getElementById("message");
+  const limit = 160; //height limit in px
+
+  textarea.oninput = function () {
+    textarea.style.height = "";
+    textarea.style.height = `${Math.min(textarea.scrollHeight, limit)}px`;
+  };
 });
