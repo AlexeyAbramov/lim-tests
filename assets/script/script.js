6c022c589f240aae01e3600750776f5edd83e4f4
AlexeyAbramov <abramov.alexey.93@ya.ru>
2021-08-31 00:12:51 +0300

    some fixes

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,12 +2,20 @@ $(document).ready(function () {
   $(".body-award__wrapper").on("click", function () {
     $(".answers").toggleClass("show");
   });
-
-  const textarea = document.getElementById("message");
-  const limit = 160; //height limit in px
-
-  textarea.oninput = function () {
-    textarea.style.height = "";
-    textarea.style.height = `${Math.min(textarea.scrollHeight, limit)}px`;
-  };
+  $(".completed-slick").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,  // swap slides in ms
+    speed: 700, // swap speed
+  });
 });
+const textarea = document.getElementById("message");
+const limit = 160; //height limit in px
+textarea.oninput = function () {
+  textarea.style.height = "";
+  textarea.style.height = `${Math.min(textarea.scrollHeight, limit)}px`;
+};
